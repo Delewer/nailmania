fe361f2a57753a79b4dc2e7de6ebdd76a54adbe7
ArturTI225 <andugd67@gmail.com>
2026-07-11 13:45:20 +0300

    Retry when app hydration does not mount

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,20 @@ import './styles.css'
 import './components.css'
 import './pages.css'
 
+function ReadyMarker(){
+  React.useEffect(()=>{
+    if(typeof window.__NM_MARK_READY__ === 'function'){
+      window.__NM_MARK_READY__();
+    }else{
+      window.__NM_APP_READY__ = true;
+    }
+  }, []);
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ReadyMarker/>
     <App/>
   </React.StrictMode>
 )
